6af2634239bcba4cb423d1cb890c1b48d7aec01b
AviPeltz <aj.peltz@gmail.com>
2024-06-10 20:41:51 -0700

    navbar full width works

--- portfolio/src/app/ui/nav-links.tsx
+++ portfolio/src/app/ui/nav-links.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import navBrand from '../../../public/NavBrand.png';
 import { useState } from 'react';
+import { usePathname } from 'next/navigation'
 
 // // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -16,19 +17,8 @@ const links =[
 ];
 
 export default function NavBar() {
-    const [selected, setSelected] = useState(links);
+    const pathname = usePathname()
 
-    function handleClick(e : any) {
-        const key = e.target.key;
-        const newLinks = selected.map(link => {
-            if (link.id === key) {
-                return {...link, isSelected: true};
-            } else {
-                return {...link, isSelected: false};
-            }
-        });
-        setSelected(newLinks);
-    }
     return(
         <div className={styles.navbar}>
             <div className={styles.navBrand}>
@@ -36,18 +26,39 @@ export default function NavBar() {
                         <Image src={navBrand} alt="navBrand" className={styles.navBrand}/>
                 </Link>
             </div>
-            <div className={styles.navlinks}>
+            <div className={styles.nav}>
+                <div className={`${styles.navLink} ${pathname === '/' ? styles.active : ''}`}>
+                    <Link className={`${styles.navLink} ${pathname === '/' ? styles.active : ''}`} href="/">
+                        About
+                    </Link>
+                </div>
+                <div className={`${styles.navLink} ${pathname === '/projects' ? styles.active : ''}`}>
+                    <Link className={`${styles.navLink} ${pathname === '/projects' ? styles.active : ''}`} href="/projects">
+                        Projects
+                    </Link>
+                </div>
+                <div className={`${styles.navLink} ${pathname === '/blog' ? styles.active : ''}`}>
+                    <Link className={`${styles.navLink} ${pathname === '/blog' ? styles.active : ''}`} href="/blog">
+                        Blog
+                    </Link>
+                </div>
+                <div className={`${styles.navLink} ${pathname === '/contact' ? styles.active : ''}`}>
+                    <Link className={`${styles.navLink} ${pathname === '/contact' ? styles.active : ''}`} href="/contact">
+                        Contact
+                    </Link>
+                </div>
+                {/*
                 {links.map(link => (
                     link.isSelected ?
                     <div className={styles.navlinkSelected} key={link.id}>
                         <Link key={link.id} href={link.href} className={styles.navlinkSelected}>{link.name}</Link> 
                      </div>
                      : 
-                     <div key={link.id} className={styles.navlinkUnselected}>
+                     <div key={link.id} className={styles.navlinkUnselected} >
                         <Link onClick={handleClick} key={link.id} href={link.href} className={styles.navlinkUnselected}>{link.name}</Link> 
                      </div>
                 ))}
-
+                */}
             </div>
         </div>
     );
